ab0da24a6716f49234c23bc6c8347726302824e3
Rafael Bernabeu <rbbernabeu@gmail.com>
2023-04-18 22:53:51 -0300

    resolve 5-instanbul-bazaar

---
+++ google/beginners-quest/5-istanbul-bazaar/decode.py
@@ -0,0 +1,21 @@
+from randcrack import RandCrack
+import struct
+
+rc = RandCrack()
+
+file = open("robo_numbers_list.txt", "rt").read().replace("-", "")
+secret = open("secret.enc", "rb").read()
+
+for f in file.splitlines():
+    rc.submit(int(f) - (1 << 31))
+
+flag = ""
+for s in secret:
+    flag += chr(s ^ rc.predict_getrandbits(8))
+
+print(flag)
+
+
+
+
+
