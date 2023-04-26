7d85671e2113ce2d0a41a3ba6fb8633fad39ecbd
Rafael Bernabeu <rbbernabeu@gmail.com>
2023-04-26 00:30:36 -0300

    challenge 17

--- google/beginners-quest/17-unknown-inside-secret-base/tester.py
+++ google/beginners-quest/17-unknown-inside-secret-base/tester.py
@@ -4,7 +4,7 @@ import os
 from encoder_golfed import encode
 from decoder import decode
 
-for i in range(1000):
+for i in range(5):
   sz = random.randint(0, 800)
   data = os.urandom(sz)
   data_processed = decode(encode(data))
